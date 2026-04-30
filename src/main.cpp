39d00283885f1e9be8c5e1da128563ec4e35a2ac
Kakicrypto <mr.chichi95@gmail.com>
2026-04-30 17:16:02 +0200

    feat: splash screen logo Kaki3D au démarrage

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <TFT_eSPI.h>
+#include "logo_kaki3d.h"
 #define TFT_PWR_EN 15
 #define TFT_BACKLIGHT 38
 TFT_eSPI tft;  // on crée l'objet écran
@@ -12,6 +13,8 @@ void setup() {
     digitalWrite(TFT_BACKLIGHT, HIGH); 
     tft.init();
     tft.setRotation(1);
+    tft.pushImage(0, 0, LOGO_WIDTH, LOGO_HEIGHT, logo_kaki3d);
+    delay(5000); // splash screen 3 secondes
     tft.fillScreen(TFT_BLACK);
     tft.setTextColor(TFT_WHITE);
     tft.setTextSize(2);
