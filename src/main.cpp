dcec7cc978c5a3da1734b74aa01111cce86dcf62
Kakicrypto <mr.chichi95@gmail.com>
2026-04-30 16:02:47 +0200

    feat: setup initial balance connectée - TFT Hello World validé

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+#include <Arduino.h>
+#include <TFT_eSPI.h>
+#define TFT_PWR_EN 15
+#define TFT_BACKLIGHT 38
+TFT_eSPI tft;  // on crée l'objet écran
+
+void setup() {
+    pinMode(TFT_PWR_EN, OUTPUT);
+    digitalWrite(TFT_PWR_EN, HIGH);
+    delay(500);
+    pinMode(TFT_BACKLIGHT, OUTPUT);
+    digitalWrite(TFT_BACKLIGHT, HIGH); 
+    tft.init();
+    tft.setRotation(1);
+    tft.fillScreen(TFT_BLACK);
+    tft.setTextColor(TFT_WHITE);
+    tft.setTextSize(2);
+    tft.setCursor(10, 10);
+    tft.println("Hello Kaki3D !");
+}
+
+void loop() {}
